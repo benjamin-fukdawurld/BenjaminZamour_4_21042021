35eac60d4767cb81da81379646a83b93cb008261
benjamin <benjamin.zamour@gmail.com>
2021-05-29 02:36:28 +0200

    Add contact page data

---
+++ src/resources/pages/contact.js
@@ -0,0 +1,11 @@
+const data = require("./contact_data");
+
+exports.name = "contact";
+exports.data = {
+    keywords: "",
+    description: "",
+    title: "Nous contacter - La Chouette Agence",
+    minified: true,
+    contentTemplate: "pages/contact/contact",
+    contentData: data,
+};
