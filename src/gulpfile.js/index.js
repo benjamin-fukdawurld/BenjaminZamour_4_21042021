3cc7ee8db426d82b92292de6c4a1272c1cdf97a7
benjamin <benjamin.zamour@gmail.com>
2021-05-29 02:55:07 +0200

    Fix Css minificaiton + Add dotenv + Add output folder as env var + add copy fonts and favicon + Add watch

--- src/gulpfile.js/index.js
+++ src/gulpfile.js/index.js
@@ -1,31 +1,71 @@
-const { src, dest, parallel } = require("gulp");
-const minify = require("gulp-minify");
+require("dotenv").config();
+
+const { src, dest, parallel, watch } = require("gulp");
+const cleanCSS = require("gulp-clean-css");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 
-const generatePages = require("./generatePages");
+const { generatePages } = require("./generatePages");
+
+process.env.distFolder = process.env.distFolder ?? "./dist";
 
 function minifyCss() {
     return src("./resources/css/*.css")
-        .pipe(minify())
+        .pipe(cleanCSS({}))
         .pipe(rename({ extname: ".min.css" }))
-        .pipe(dest("./test/css"));
+        .pipe(dest(process.env.distFolder + "/css"));
 }
 
 function minifyJs() {
     return src("./resources/js/*.js")
         .pipe(uglify())
         .pipe(rename({ extname: ".min.js" }))
-        .pipe(dest("./test/js"));
+        .pipe(dest(process.env.distFolder + "/js"));
 }
 
 function optimiseImages() {
-    return src("./resources/img/*").pipe(imagemin()).pipe(dest("./test/img"));
+    return src("./resources/img/*")
+        .pipe(imagemin())
+        .pipe(dest(process.env.distFolder + "/img"));
+}
+
+function copyFavIcon() {
+    return src("./resources/favicon.*").pipe(dest(process.env.distFolder));
+}
+
+function copyFonts() {
+    return src("./resources/fonts/*").pipe(
+        dest(process.env.distFolder + "/fonts")
+    );
 }
 
 exports.optimiseImages = optimiseImages;
+exports.copyImages = parallel(optimiseImages, copyFavIcon);
 exports.minifyCss = minifyCss;
 exports.minifyJs = minifyJs;
-exports.generatePages = generatePages.generatePages;
-exports.default = parallel(optimiseImages, generatePages.generatePages, minifyCss, minifyJs);
+exports.copyFonts = copyFonts;
+exports.generatePages = generatePages;
+exports.default = parallel(
+    exports.copyImages,
+    generatePages,
+    minifyCss,
+    minifyJs,
+    copyFonts
+);
+
+exports.watch = function () {
+    watch("./resources/img/*", { ignoreInitial: false }, optimiseImages);
+
+    watch("./resources/favicon.*", { ignoreInitial: false }, copyFavIcon);
+
+    watch("./resources/js/*.js", { ignoreInitial: false }, minifyJs);
+
+    watch("./resources/css/*.css", { ignoreInitial: false }, minifyCss);
+
+    watch("./resources/fonts/*", { ignoreInitial: false }, copyFonts);
+
+    watch("./resources/pages/*", { ignoreInitial: false }, generatePages);
+
+    watch("./resources/templates/**", { ignoreInitial: false }, generatePages);
+};
