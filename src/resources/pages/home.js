15a468a9d51950a3c3627b1670750a973e6d3e5e
benjamin <benjamin.zamour@gmail.com>
2021-06-07 11:38:04 +0200

    Fix main page title

--- src/resources/pages/home.js
+++ src/resources/pages/home.js
@@ -5,7 +5,7 @@ exports.data = {
     keywords: "",
     description:
         "Besoin d'un site internet ? Nous sommes à votre service pour vous fournir une solution clé en main personnalisée pour votre stratégie marketing digital",
-    title: "Entreprise de web design à Lyon | La Chouette Agence",
+    title: "Entreprise de web design à Lyon - La Chouette Agence",
     minified: true,
     contentTemplate: "pages/home/home",
     contentData: { services, projects },
